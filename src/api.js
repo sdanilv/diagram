67e878c42e374217d07d326daec74505287e6752
sdanilv <sdanilv@gmail.com>
2020-08-10 22:57:59 +0300

    add service

--- src/api.js
+++ src/api.js
@@ -1,26 +1,18 @@
 import axios from "axios";
-import { WEEK } from "./tools/constant";
+import {MONTH, WEEK} from './tools/constant';
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3004/",
 });
 
 export const fetchImpulsesForType = async (type = "month") => {
-  let impulses = [];
-  if (type === WEEK) {
-    const { data } = await axiosInstance.get(
-      `month?_sort=id&_order=desc&_limit=14`
-    );
-    impulses = data.reverse();
-  } else {
-    const { data } = await axiosInstance.get(`${type}`);
-    impulses = data;
-  }
-  return impulses;
+  const { data } = await axiosInstance.get(`${type===WEEK?MONTH:type}`);
+
+  return data;
 };
 export const fetchImpulsesInRange = async (from, to) => {
   console.log(`range?from=${from}&to=${to}`);
-  const { data } = await axiosInstance.get(`month?_start=${10}&_end=${20}`);
+  const { data } = await axiosInstance.get(`month`);
   // const { data } = await axiosInstance.get(`range?from=${from}&to=${to}`);
   return data;
 };
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,49 +1,60 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {fetchImpulsesForType, fetchImpulsesInRange} from "../api";
-import {MONTH, WEEK} from "../tools/constant";
-import {formattedDate} from "../tools/FomatedDate";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchImpulsesForType, fetchImpulsesInRange } from "../api";
+import { MONTH, WEEK } from "../tools/constant";
+import { formattedDate } from "../tools/FomatedDate";
 
 const chartSlice = createSlice({
-    name: "chart",
-    initialState: {
-        impulses: [],
-        totalCount: 0,
-        totalSum: 0
+  name: "chart",
+  initialState: {
+    services: [],
+    checked: [],
+    totalCount: 0,
+    totalSum: 0,
+    charData:[]
+  },
+  reducers: {
+    loadImpulses(state, { payload }) {
+      state.services = payload;
     },
-    reducers: {
-        loadImpulses(state, {payload}) {
-            state.impulses = payload.impulses;
-            state.totalCount = payload.totalCount;
-            state.totalSum = payload.totalSum;
-        },
-    },
+    chartDate(state, { payload }){
+      state.totalCount = payload.totalCount;
+      state.totalSum = payload.totalSum;
+      state.charData = payload.data
+    }
+  },
 });
 
-const {actions, reducer} = chartSlice;
-const {loadImpulses} = actions;
+const { actions, reducer } = chartSlice;
+const { loadImpulses } = actions;
 export default reducer;
 
 export const fetchImpulses = (type = WEEK) => async (dispatch) => {
-    let impulses = await fetchImpulsesForType(type);
-    impulses = impulses.map((imp) => ({
-        ...imp,
-        sum: +imp.sum,
-        count: +imp.count,
-        date: formattedDate(type, imp.date),
-    }));
-  const [totalCount, totalSum] = impulses.reduce((accum, imp) => [accum[0] + imp.count, accum[1] + imp.sum], [0, 0]);
-console.log(totalCount)
-    dispatch(loadImpulses({impulses,totalCount, totalSum }));
+  let impulses = await fetchImpulsesForType(type);
+  impulses = impulses.map((imp) => ({
+    ...imp,
+    sum: +imp.sum,
+    count: +imp.count,
+    date: formattedDate(type, imp.date),
+  }));
+
+  const [totalCount, totalSum] = impulses.reduce(
+    (accum, imp) => [accum[0] + imp.count, accum[1] + imp.sum],
+    [0, 0]
+  );
+  console.log(totalCount);
+  dispatch(loadImpulses({ impulses, totalCount, totalSum }));
 };
 
-export const loadImpulsesInRange = (from, to) => async (dispatch) => {
-    let impulses = await fetchImpulsesInRange(from, to);
-    impulses = impulses.map((imp) => ({
-        ...imp,
-        sum: +imp.sum,
-        count: +imp.count,
-        date: formattedDate(MONTH, imp.date),
-    }));
 
-    dispatch(loadImpulses(impulses));
+
+export const loadImpulsesInRange = (from, to) => async (dispatch) => {
+  let impulses = await fetchImpulsesInRange(from, to);
+  impulses = impulses.map((imp) => ({
+    ...imp,
+    sum: +imp.sum,
+    count: +imp.count,
+    date: formattedDate(MONTH, imp.date),
+  }));
+
+  dispatch(loadImpulses(impulses));
 };
